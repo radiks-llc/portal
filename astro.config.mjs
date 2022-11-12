98e6102143f4cf265f3684bf5333c9ff7701c4a0
Mitchell Hynes <ecumene@users.noreply.github.com>
2022-11-12 16:26:53 -0330

    Yuck yuc kyuck

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import node from '@astrojs/node';
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: cloudflare()
 });
